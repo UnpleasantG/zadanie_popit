959c7a3eefe6714b7e43d89a121dcc8a7f18b17d
SK8R <coool.ranboo@gmail.com>
2025-11-11 11:48:37 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct Person
+{
+    string Name;
+    string Family;
+};
+
+int main()
+{
+    string path = "C:\\programming\\ooo.txt";
+
+    ifstream file;
+    file.open(path);
+
+    if (!file.is_open())
+    {
+        cout << "File could not be opened" << endl;
+        return -1;
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        int pos = line.find('|');
+        Person person;
+        person.Family = line.substr(0, pos);
+        person.Name = line.substr(pos + 1);
+
+        string path2 = "C:\\programming\\eee.txt";
+
+        ofstream file2;
+        file2.open(path2, ios::app);
+
+        if (file2.is_open())
+           {
+            file2 << person.Family << endl;
+            file2.close();
+           }
+        
+    }
+    file.close();
+
+    return 0;
+    
+}
+
